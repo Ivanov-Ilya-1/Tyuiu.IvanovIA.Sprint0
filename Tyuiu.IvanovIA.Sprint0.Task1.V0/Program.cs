34865d32ce094c77e2fd012397969dac2636aaa7
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-08 11:37:26 +0300

    Task1

---
+++ Tyuiu.IvanovIA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tyuiu.IvanovIA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+            string name = "Илья";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, мне " + age + " лет, а зовут меня " + name );
+            Console.ReadKey();
+        }
+    }
+}
