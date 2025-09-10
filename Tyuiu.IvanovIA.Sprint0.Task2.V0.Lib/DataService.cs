551879bb950bb971b7b4e29d581629a54e10d8e0
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-10 20:16:42 +0300

    Task2

---
+++ Tyuiu.IvanovIA.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace Tyuiu.IvanovIA.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+
+        public static string GetMessage(string name)
+        {
+
+            return $"Привет, {name}";
+
+        }
+
+
+
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+using Tyuiu.IvanovIA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IvanovIA.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DataService.GetMessage("Илья"));
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+
+
+}
