7e72bf735f99d7d17218d1b1e7cfa520098f5a36
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-10 19:32:51 +0300

    Task1

--- Tyuiu.IvanovIA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.IvanovIA.Sprint0.Task1.V0/Program.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.IvanovIA.Sprint0.Task1.V0
         {
 
             Console.WriteLine("Hello, World!");
-            string name = "Паша";
+            string name = "Илья";
             int age = 18;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет." );
             Console.ReadKey();
