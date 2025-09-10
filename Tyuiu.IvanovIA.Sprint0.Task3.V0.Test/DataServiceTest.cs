077aef10e4f6de234aa82b75297516b80d5779be
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-10 20:55:34 +0300

    Task3

---
+++ Tyuiu.IvanovIA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tyuiu.IvanovIA.Sprint0.Task3.V0.Lib;
+
+
+
+namespace Tyuiu.IvanovIA.Sprint0.Task3.V0.Test
+{
+    public class DataServiceTest
+    {
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
---
+++ Tyuiu.IvanovIA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tyuiu.IvanovIA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.IvanovIA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
