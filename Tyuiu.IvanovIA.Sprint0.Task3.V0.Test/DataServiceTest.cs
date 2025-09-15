8bf795ed3e20c2b1de06860b5e90942867efb6e4
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-15 10:51:16 +0300

    Task4

--- Tyuiu.IvanovIA.Sprint0.Task3.V0.Test/DataServiceTest.cs
+++ Tyuiu.IvanovIA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -6,12 +6,15 @@ using Tyuiu.IvanovIA.Sprint0.Task3.V0.Lib;
 namespace Tyuiu.IvanovIA.Sprint0.Task3.V0.Test
 {
     [TestClass]
+    public sealed class DataServiceTest
     {
+        [TestMethod]
         public void CheckedValid()
         {
             Assert.AreEqual(10, DataService.Sum(5, 5));
 
+        }
+
+
     }
-
-
 }
