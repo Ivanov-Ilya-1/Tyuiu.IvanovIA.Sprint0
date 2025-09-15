a70ccb9f98d1767553ef6ef914ebbcf79fdcac37
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-15 12:32:32 +0300

    Task7

---
+++ Tyuiu.IvanovIA.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.IvanovIA.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.IvanovIA.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionalArraysValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 1, 1, 1, 1, 1 };
+            int[] nums3 = { 2, 2, 2, 2, 2 };
+
+
+
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3,res);
+        }
+    }
+}
